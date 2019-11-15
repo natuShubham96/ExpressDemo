f2814289b7c2e7f1ae40207afe306734606b620d
shubhamnatu <shubham.natu@cognizant.com>
2019-11-15 23:55:46 +0530

    Up to date demo

---
+++ index.js
@@ -0,0 +1,67 @@
+
+//Importing express
+
+const express = require('express')
+const app = express()
+
+//Third party middleware
+
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+//Custom Middleware
+
+const logger = require('./logger')
+const auth = require('./auth')
+
+const testData = [
+    {id:1, name:"asd"}
+]
+
+// console.log(`Production or dev????? ${process.env.NODE_ENV}`)
+
+// console.log(`Environment ${app.get('env')}`)
+
+//Using middlewares
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+app.use(helmet())
+
+//to check development or production environment
+
+let environment = app.get('env')
+
+if(environment === 'development')
+app.use(morgan('tiny'))
+
+app.use(logger)
+
+app.use(auth)
+
+//Hitting the apis
+
+app.get('/api/test', (req,res) => {
+    res.send(testData)
+})
+
+app.post('/api/test', (req,res) => {
+    const data ={
+        id: testData.length +1,
+        name: req.body.name
+    }
+    testData.push(data)
+    console.log(req.query)
+    res.send(data)
+})
+
+//Port related operations
+
+const port = process.env.port || 3000
+
+app.listen(port, () => console.log("Listening...."))
+
